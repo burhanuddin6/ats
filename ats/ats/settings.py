a74b602dddceae01f7e2e83740afce7308736435
Burhanuddin <be07724@st.habib.edu.pk>
2023-08-01 14:25:04 +0500

    Collected static files into BASE_DIR + static.

--- ats/ats/settings.py
+++ ats/ats/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-iwz=b1)t)c_u(q$*$ugf2&1z6k3$wo2sa=d=n-dr)**)st03gt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['burhanuddin0.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,8 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# just use it in development to collect static files
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
